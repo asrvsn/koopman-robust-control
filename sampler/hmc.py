organize sampler; fix signs

sampler/hmc.py
@@ -13,11 +13,13 @@ def gibbs(params: tuple):
 	distributions = tuple(torch.distributions.Normal(torch.zeros_like(w), torch.ones_like(w)) for w in params)
 	return tuple(d.sample() for d in distributions)
 
-def leapfrog(params: tuple, momentum: tuple, potential: Callable, boundary: Callable, n_leapfrog: int, step_size: float):
+def leapfrog(params: tuple, momentum: tuple, potential: Callable, boundary: Callable, n_leapfrog: int, step_size: float, zero_nan=True):
 	def params_grad(p):
 		p = tuple(w.detach().requires_grad_() for w in p)
 		u = potential(p)
 		d_p = torch.autograd.grad(u, p)
+		if zero_nan: 
+			d_p = tuple(torch.zeros_like(dw) if torch.isnan(dw).any() else dw for dw in d_p)
 		return d_p
 
 	momentum = zip_with(momentum, params_grad(params), lambda m, dp: m - 0.5*step_size*dp)
@@ -25,11 +27,11 @@ def leapfrog(params: tuple, momentum: tuple, potential: Callable, boundary: Call
 	for n in range(n_leapfrog):
 
 		# Reflect particle until not in violation of boundary 
-		bc = boundary(params, momentum, step_size, resolution=20)
+		bc = boundary(params, momentum, step_size)
 		while bc is not None: 
 			print('Reflected!')
 			(params, momentum) = bc
-			bc = boundary(params, momentum, step_size, resolution=20)
+			bc = boundary(params, momentum, step_size)
 
 		params = zip_with(params, momentum, lambda p, m: p + step_size*m)
 		momentum = zip_with(momentum, params_grad(params), lambda m, dp: m - step_size*dp)

systems/vdp.py
@@ -9,12 +9,13 @@ def system(mu: float):
 def dataset(mu: float, a=0, b=10, n=500, skip=0):
 	t = linspace(a, b, n)
 	sol = solve_ivp(system(mu), [a, b], [1, 0], t_eval=t)
-	X, Y = sol.y[:, skip:-1], sol.y[:, skip+1:] # returns: d x n 
+	X, Y = sol.y[:, skip:-1], sol.y[:, skip+1:] 
 	return torch.from_numpy(X).float(), torch.from_numpy(Y).float()
 
 if __name__ == '__main__':
 	mu = 3
-	X, Y = dataset(mu)
+
+	X, Y = dataset(mu, b=20, n=2000, skip=400)
 	print(X.shape, Y.shape)
 	plt.figure(figsize=(8,8))
 	plt.plot(X[0], X[1])

utils.py
@@ -16,7 +16,7 @@ def zip_with(X: tuple, Y: tuple, f: Callable):
 def eig(A, v):
 	return torch.mm(v.t(), torch.mm(A, v))
 
-def spectral_radius(A: torch.Tensor, eps=1e-3):
+def spectral_radius(A: torch.Tensor, eps=1e-6):
 	d = A.shape[0]
 	v = torch.ones((d,1), device=A.device) / np.sqrt(d)
 	ev = eig(A, v)

vdp_uncertain.py
@@ -5,23 +5,21 @@ from features import *
 from kernel import *
 from operators import *
 from utils import *
-import hmc as hmc
+from sampler.dynamics import perturb
 import systems.vdp as vdp
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-torch.autograd.set_detect_anomaly(True)
+# torch.autograd.set_detect_anomaly(True)
 
 set_seed(9001)
 
 # Init features
 p, d, k = 4, 2, 8
 obs = PolynomialObservable(p, d, k)
-# tau = 10
-# obs = DelayObservable(tau) # Delay observable is not predictive, causes NaN
 
 # Init data
 mu = 3.0
-X, Y = vdp.dataset(mu, n=2000, b=20)
+X, Y = vdp.dataset(mu, n=8000, b=20) # start on limit cycle
 X, Y = X.to(device), Y.to(device)
 PsiX, PsiY = obs(X), obs(Y)
 
@@ -34,95 +32,40 @@ P0 = dmd(PsiX, PsiY)
 P0 = P0.to(device)
 assert not torch.isnan(P0).any().item()
 
-# HMC
+# Sample dynamics
 
 baseline = False
-eig_max = spectral_radius(P0)
-s_max, s_min = eig_max, eig_max - 1e-3
+beta = 100
 dist_func = (lambda x, y: torch.norm(x - y)) if baseline else (lambda x, y: K(x, y, normalize=True)) 
 
-alpha, beta = 1, 20
-pdf = torch.distributions.beta.Beta(torch.Tensor([alpha]).to(device), torch.Tensor([beta]).to(device))
-
-def potential(params: tuple):
-	(P,) = params
-	d_k = dist_func(P0, P).clamp(1e-6, 1-1e-6)
-	print(d_k.item())
-	u = -pdf.log_prob(d_k)
-	return u
-
-def boundary(params: tuple, momentum: tuple, step: float, resolution=10):
-	(P,) = params
-	(M,) = momentum
-	s = spectral_radius(P + step*M)
-	if s > s_max:
-		micro_step = step/resolution
-		P_cand = P.detach()
-		while spectral_radius(P_cand) <= s_max:
-			P_cand += micro_step*M
-		P_cand = P_cand.requires_grad_(True)
-		# Reflect along plane orthogonal to spectral gradient
-		dS = -torch.autograd.grad(spectral_radius(P_cand), P_cand)[0]
-		M_para = torch.trace(torch.mm(M.t(), dS)) * dS / torch.trace(torch.mm(dS.t(), dS))
-		M_refl = M - 2*M_para
-		return ((P_cand,), (M_refl,))
-	elif s < s_min:
-		micro_step = step/resolution
-		P_cand = P.detach()
-		while spectral_radius(P_cand) >= s_min:
-			P_cand += micro_step*M
-		P_cand = P_cand.requires_grad_(True)
-		# Reflect along plane orthogonal to spectral gradient
-		dS = torch.autograd.grad(spectral_radius(P_cand), P_cand)[0]
-		M_para = torch.trace(torch.mm(M.t(), dS)) * dS / torch.trace(torch.mm(dS.t(), dS))
-		M_refl = M - 2*M_para
-		return ((P_cand,), (M_refl,))
-	return None
-
-samples, ratio = hmc.sample(10, (P0,), potential, boundary, n_leapfrog=25, step_size=.0001)
-samples = [P.detach() for (P,) in samples]
-
-print('Acceptance ratio: ', ratio)
-print('Nominal spectral norm:', eig_max.item())
-print('Perturbed spectral norms:', [spectral_radius(P).item() for P in samples])
+samples = perturb(
+	10, P0, dist_func, beta,  
+	sp_div=(1e-3, 1e-3),
+	hmc_step=1e-4,
+	hmc_leapfrog=25,
+)
 
 # Save samples
+
 name = 'perturbed_baseline' if baseline else 'perturbed_pf' 
 torch.save(torch.stack(samples), f'tensors/{name}_vdp.pt')
 
 # Visualize perturbations
 
-t = 2000
-# t = X.shape[1]
-
-# plt.figure()
-# plt.title('Nominal prediction')
-# Z0 = obs.preimage(torch.mm(P0, obs(X))).cpu()
-# plt.plot(Z0[0], Z0[1])
+t = 8000
 
 plt.figure()
 plt.xlim(left=-6.0, right=6.0)
 plt.ylim(bottom=-6.0, top=6.0)
 plt.title(f'Perturbations of Van der Pol ({"baseline" if baseline else "kernel"})')
-Z0 = obs.extrapolate(P0, X, t).cpu()
+Z0 = obs.extrapolate(P0.cpu(), X.cpu(), t)
 plt.plot(Z0[0], Z0[1], label='Nominal')
 
 for Pn in samples:
-	# Zn = obs.preimage(torch.mm(Pn, obs(X))).cpu()
-	Zn = obs.extrapolate(Pn, X, t).cpu()
+	Zn = obs.extrapolate(Pn.cpu(), X.cpu(), t)
 	plt.plot(Zn[0], Zn[1], color='orange')
 
 plt.legend()
-
-# for _ in range(3):
-# 	sigma = 0.1
-# 	eps = torch.distributions.Normal(torch.zeros_like(P0, device=device), torch.full(P0.shape, sigma, device=device)).sample()
-# 	Pn = P0 + eps
-# 	# Zn = obs.preimage(torch.mm(Pn, obs(X))).cpu()
-# 	Zn = extrapolate(Pn, x_0, obs, t).cpu()
-# 	plt.figure()
-# 	plt.title('Perturbed extrapolation (Fro distance)')
-# 	plt.plot(Zn[0], Zn[1])
 
 plt.show()
 