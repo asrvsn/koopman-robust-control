a69694e6ae8f10fefff745459864ab3cb5872cd1
Anand <anand@alphasheets.com>
2020-03-18 21:30:40 -0400

    optimize boundary conditions

--- sampler/dynamics.py
+++ sampler/dynamics.py
@@ -11,7 +11,7 @@ from sampler.utils import *
 def perturb(
 		n_samples: int, P0: torch.Tensor, dist_func: Callable, beta: float, 
 		sp_div=(1e-3, 1e-3), alpha=1, 
-		hmc_step=0.0001, hmc_random_step=False, hmc_leapfrog=25, hmc_burn=20, hmc_boundary_resolution=20, 
+		hmc_step=0.0001, hmc_random_step=False, hmc_leapfrog=25, hmc_burn=20,
 		hmc_target_accept=0.75, NUTS=False 
 	):
 	'''
@@ -24,7 +24,6 @@ def perturb(
 	alpha: leave at 1.
 	hmc_step: step size for HMC, initial step size for NUTS HMC
 	hmc_burn: samples discarded during burn-in phase
-	hmc_boundary_resolution: step-size divisor for identifying boundary locations
 	hmc_target_accept: target accept rate for NUTS
 	NUTS: use No U-Turn Sampler 
 	'''
@@ -43,37 +42,7 @@ def perturb(
 		u = -pdf.log_prob(d_k)
 		return u
 
-	def boundary(params: tuple, momentum: tuple, step: float):
-		(P,) = params
-		(M,) = momentum
-		s = spectral_radius(P + step*M)
-		if s > s_max:
-			# print('Upper boundary')
-			micro_step = step/hmc_boundary_resolution
-			P_cand = P.detach()
-			while spectral_radius(P_cand) <= s_max:
-				# print('Upper reflection:', spectral_radius(P_cand).item())
-				P_cand += micro_step*M
-			P_cand = P_cand.requires_grad_(True)
-			# Reflect along plane orthogonal to spectral gradient
-			dS = torch.autograd.grad(spectral_radius(P_cand), P_cand)[0]
-			M_para = (torch.trace(M.t()@dS) / torch.trace(dS.t()@dS)) * dS 
-			M_refl = M - 2*M_para
-			return ((P_cand,), (M_refl,))
-		elif s < s_min:
-			# print('Lower boundary')
-			micro_step = step/hmc_boundary_resolution
-			P_cand = P.detach()
-			while spectral_radius(P_cand) >= s_min:
-				# print('Lower reflection:', spectral_radius(P_cand).item())
-				P_cand += micro_step*M
-			P_cand = P_cand.requires_grad_(True)
-			# Reflect along plane orthogonal to spectral gradient
-			dS = -torch.autograd.grad(spectral_radius(P_cand), P_cand)[0]
-			M_para = (torch.trace(M.t()@dS) / torch.trace(dS.t()@dS)) * dS 
-			M_refl = M - 2*M_para
-			return ((P_cand,), (M_refl,))
-		return None
+	boundary = fn_boundary(spectral_radius, vmin=s_max, vmax=s_min)
 
 	print('Sampling...')
 
@@ -89,3 +58,7 @@ def perturb(
 	print('Perturbed spectral radii:', [spectral_radius(P).item() for P in samples])
 
 	return samples
+
+if __name__ == '__main__':
+	# TODO tests
+	pass
--- sampler/reflection.py
+++ sampler/reflection.py
@@ -1,5 +1,8 @@
-''' Boundary helper functions ''' 
+''' 
+Boundary helper functions 
+''' 
 
+from typing import Callable
 import torch
 
 def rect_boundary(vmin=-float('inf'), vmax=float('inf')):
@@ -27,8 +30,38 @@ def rect_boundary(vmin=-float('inf'), vmax=float('inf')):
 		return None
 	return boundary
 
-def fn_boundary():
+def fn_boundary(fn: Callable, vmin=-float('inf'), vmax=float('inf'), boundary_resolution=10):
 	'''
 	1D boundary imposed on differentiable scalar function of parameters.
 	'''
-	pass
+	assert vmin < vmax
+	def boundary(params: tuple, momentum: tuple, step: float):
+		p, m = params[0], momentum[0] # TODO generalize to n-d
+		v = fn(p)
+
+		if v > vmax:
+			micro_step = step/boundary_resolution
+			p_cand = p.detach()
+			while fn(p) < vmax:
+				p_cand += micro_step*m
+			p_cand = p_cand.requires_grad_()
+			# Reflect along plane orthogonal to gradient
+			grad = torch.autograd.grad(fn(p_cand), p_cand)[0]
+			m_para = (torch.trace(m.t()@grad) / torch.trace(grad.t()@grad)) * grad
+			m_refl = m - 2*m_para
+			return ((p_cand.detach().requires_grad_(),), (m_refl.detach().requires_grad_(),))
+
+		elif v < vmin:
+			micro_step = step/boundary_resolution
+			p_cand = p.detach()
+			while fn(p) > vmin:
+				p_cand += micro_step*m
+			p_cand = p_cand.requires_grad_()
+			# Reflect along plane orthogonal to gradient
+			grad = -torch.autograd.grad(fn(p_cand), p_cand)[0]
+			m_para = (torch.trace(m.t()@grad) / torch.trace(grad.t()@grad)) * grad
+			m_refl = m - 2*m_para
+			return ((p_cand.detach().requires_grad_(),), (m_refl.detach().requires_grad_(),))
+
+		return None
+	return boundary
