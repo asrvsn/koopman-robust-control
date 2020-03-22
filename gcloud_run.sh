6a2f57f8159781a1805bf300f182e398ca80430a
anand <asrini@mit.edu>
2020-03-22 14:34:59 -0400

    change serialization

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # set -e
-gcloud compute instances start pytorch-1-vm --zone=us-west1-b
-gcloud compute ssh pytorch-1-vm --zone=us-west1-b
-gcloud compute instances stop pytorch-1-vm --zone=us-west1-b
+gcloud compute instances start pytorch-2-vm --zone=us-west1-a
+gcloud compute ssh --ssh-flag="-Y" pytorch-2-vm --zone=us-west1-a
+# gcloud compute ssh --ssh-flag="-Y -v" pytorch-2-vm --zone=us-west1-a # If debug
+gcloud compute instances stop pytorch-2-vm --zone=us-west1-a
--- sampler/hmc_parallel.py
+++ sampler/hmc_parallel.py
@@ -18,6 +18,7 @@ def worker(
 	if seed is not None:
 		set_seed(seed)
 	samples, ratio = hmc.sample(n_samples, init_params, _implicit_potential, _implicit_boundary, step_size=step_size, n_leapfrog=n_leapfrog, n_burn=n_burn, random_step=random_step, debug=debug, return_first=return_first)
+	samples = [s.numpy() for (s,) in samples]
 	print(f'Ratio: {ratio}')
 	return samples
 
@@ -31,18 +32,20 @@ def sample(
 	_implicit_potential = potential
 	_implicit_boundary = boundary
 
-	n_problems = len(initial_conditions)
-	with multiprocessing.Pool() as pool:
-		seeds = range(1000, 1000+n_problems) if deterministic else repeat(None)
-		args = zip(
-			repeat(n_samples), initial_conditions, 
-			repeat(step_size), repeat(n_leapfrog), repeat(n_burn), repeat(random_step), repeat(debug), repeat(return_first),
-			seeds
-		)
-		samples = pool.starmap(worker, args)
+	samples = []
+	def add_samples(results: list):
+		samples.extend(results)
 
-	samples = [s for subset in samples for s in subset]
-	return samples
+	with multiprocessing.Pool() as pool:
+		for i, ic in enumerate(initial_conditions):
+			seed = 1000+i if deterministic else None
+			pool.apply_async(worker, args=(
+				n_samples, ic, step_size, n_leapfrog, n_burn, random_step, debug, return_first, seed
+			), callback=add_samples)
+		pool.close()
+		pool.join()
+
+	return np.array(samples)
 
 if __name__ == '__main__':
 	import scipy.stats as stats
@@ -53,32 +56,32 @@ if __name__ == '__main__':
 	set_mp_backend()
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-	# Reflection test
-	N = 20
-	step = 0.3
-	L = 5
-	burn = 2
+	# # Reflection test
+	# N = 20
+	# step = 0.3
+	# L = 5
+	# burn = 2
 
-	pdf = torch.distributions.uniform.Uniform(torch.Tensor([[-1.0], [-1.0]]), torch.Tensor([[1.0], [1.0]]))
-	initial_conditions = [(pdf.sample(),) for _ in range(5)]
-	potential = lambda _: 0 # uniform over [-1,1]x[-1,1]
-	boundary = reflections.lp_boundary(float('inf'), vmax=1)
+	# pdf = torch.distributions.uniform.Uniform(torch.Tensor([[-1.0], [-1.0]]), torch.Tensor([[1.0], [1.0]]))
+	# initial_conditions = [(pdf.sample(),) for _ in range(5)]
+	# potential = lambda _: 0 # uniform over [-1,1]x[-1,1]
+	# boundary = reflections.lp_boundary(float('inf'), vmax=1)
 
-	samples = sample(N, initial_conditions, potential, boundary, step_size=step, n_leapfrog=L, n_burn=burn, deterministic=True)
-	samples = np.array([s.view(-1).numpy() for (s,) in samples])
-	initial_conditions = np.array([s.view(-1).numpy() for (s,) in initial_conditions])
+	# samples = sample(N, initial_conditions, potential, boundary, step_size=step, n_leapfrog=L, n_burn=burn, deterministic=True)
+	# samples = np.array([s.view(-1).numpy() for (s,) in samples])
+	# initial_conditions = np.array([s.view(-1).numpy() for (s,) in initial_conditions])
 
-	plt.figure()
-	plt.title(f'sampler.hmc_parallel on the square, step={step}')
-	plt.scatter(initial_conditions[:,0], initial_conditions[:,1], color='orange', label='Initial condition')
-	plt.scatter(samples[:,0], samples[:,1])
-	x = np.linspace(-1,1,20)
-	ones = np.ones(x.shape)
-	plt.plot(x,ones,color='black')
-	plt.plot(ones,x,color='black')
-	plt.plot(x,-ones,color='black')
-	plt.plot(-ones,x,color='black')
-	plt.legend()
+	# plt.figure()
+	# plt.title(f'sampler.hmc_parallel on the square, step={step}')
+	# plt.scatter(initial_conditions[:,0], initial_conditions[:,1], color='orange', label='Initial condition')
+	# plt.scatter(samples[:,0], samples[:,1])
+	# x = np.linspace(-1,1,20)
+	# ones = np.ones(x.shape)
+	# plt.plot(x,ones,color='black')
+	# plt.plot(ones,x,color='black')
+	# plt.plot(x,-ones,color='black')
+	# plt.plot(-ones,x,color='black')
+	# plt.legend()
 
 	# Gaussian test
 	mean = torch.Tensor([0.,0.,0.])
@@ -89,7 +92,7 @@ if __name__ == '__main__':
 		return -pdf.log_prob(params[0].view(-1)).sum()
 
 	N = 1000
-	n_problems = 20
+	n_problems = 50
 	step = .3
 	L = 5
 	burn = 50
@@ -97,7 +100,6 @@ if __name__ == '__main__':
 	initial_conditions = [(torch.zeros((3,1)),) for _ in range(n_problems)]
 	boundary = reflections.nil_boundary
 	samples = sample(N, initial_conditions, potential, boundary, step_size=step, n_leapfrog=L, n_burn=burn, deterministic=True)
-	samples = np.array([s.view(-1).numpy() for (s,) in samples]) 
 	x = np.linspace(-6,6,200)
 	fig, axs = plt.subplots(1, 3)
 	fig.suptitle(f'sampler.hmc_parallel on 3d gaussian, var={var.numpy().tolist()}, step={step}')
