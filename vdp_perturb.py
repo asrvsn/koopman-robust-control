08c0bfbaaffa40ad810a86ae0c7bedc4fe49cf69
Anand <anand@alphasheets.com>
2020-03-23 15:42:32 -0400

    vdp

--- vdp_perturb.py
+++ vdp_perturb.py
@@ -6,7 +6,7 @@ from sampler.kernel import *
 from sampler.operators import *
 from sampler.utils import *
 from sampler.dynamics import *
-import systems.duffing as duffing
+import systems.vdp as vdp
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # torch.autograd.set_detect_anomaly(True)
@@ -14,24 +14,15 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 set_seed(9001)
 
 # Init features
-p, d, k = 5, 2, 15
+p, d, k = 4, 2, 8
 obs = PolynomialObservable(p, d, k)
 
-# Init data 
-t_max = 400
-n_per = 16000
-n_init = 12
-x0s = np.linspace(-2.0, 2.0, n_init)
-xdot0s = np.linspace(-2.0, 2.0, n_init)
-X, Y = [], []
-for x0 in x0s:
-	for xdot0 in xdot0s:
-		# Unforced duffing equation
-		Xi, Yi = duffing.dataset(t_max, n_per, gamma=0.0, x0=x0, xdot0=xdot0)
-		X.append(Xi)
-		Y.append(Yi)
-X, Y = torch.cat(tuple(X), axis=1), torch.cat(tuple(Y), axis=1)
-
+# Init data
+mu = 3.0
+n_vdp = 6000
+b_vdp = 40
+skip_vdp = int(n_vdp * 8 / b_vdp) # start on limit cycle
+X, Y = vdp.dataset(mu, n=n_vdp, b=b_vdp, skip=skip_vdp) 
 X, Y = X.to(device), Y.to(device)
 PsiX, PsiY = obs(X), obs(Y)
 
@@ -41,30 +32,30 @@ nominal = nominal.to(device)
 assert not torch.isnan(nominal).any().item()
 
 # Sample dynamics
-baseline = True
+baseline = False
 
 if baseline:
 	dist_func = euclidean_matrix_kernel
 else:
-	d, m, T = PsiX.shape[0], 2, 20
+	d, m, T = PsiX.shape[0], 2, 80
 	K = PFKernel(device, d, m, T)
 	dist_func = lambda x, y: K(x, y, normalize=True) 
 
-
 beta = 5
-step = 1e-4
-x_samples = 6
-y_samples = 3
-n_samples = 100
+step=1e-4
+n_samples = 200
 n_split = 20
+burn = 5
 
-samples = perturb(n_samples, nominal, beta, dist_func=dist_func, r_div=(1e-2, 1e-2), r_step=3e-5, n_split=n_split, hmc_step=step)
+samples = perturb(n_samples, nominal, beta, dist_func=dist_func, r_div=(1e-2, 1e-2), r_step=3e-5, n_split=n_split, hmc_step=step, hmc_burn=burn)
 posterior = [dist_func(nominal, s).item() for s in samples]
 
 results = {
 	'nominal': nominal.numpy(),
 	'posterior': posterior,
 	'samples': [s.numpy() for s in samples],
+	'step': step,
+	'beta': beta,
 }
 print('Saving..')
-hkl.dump(results, f'saved/duffing{"_baseline" if baseline else ""}.hkl')
+hkl.dump(results, f'saved/vdp{"_baseline" if baseline else ""}.hkl')
---
+++ vdp_plot.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import hickle as hkl
+import random
+
+results = hkl.load('saved/duffing.hkl')
+nominal = torch.from_numpy(results['nominal']).float()
+posterior = results['posterior']
+samples = [torch.from_numpy(s).float() for s in results['samples']]
+step = results['step']
+beta = results['beta']
+
+# Visualize perturbations
+
+mu = 3.0
+n_vdp = 6000
+b_vdp = 40
+skip_vdp = int(n_vdp * 8 / b_vdp) # start on limit cycle
+X, Y = vdp.dataset(mu, n=n_vdp, b=b_vdp, skip=skip_vdp)
+t = int(n_vdp/2)
+
+fig, axs = plt.subplots(1, 2)
+
+axs[0].set_xlim(left=-7.0, right=7.0)
+axs[0].set_ylim(bottom=-7.0, top=7.0)
+axs[0].set_title(f'samples, step={step}')
+
+for Pn in random.choices(samples, k=20):
+	Zn = obs.extrapolate(Pn, X, t)
+	axs[0].plot(Zn[0], Zn[1], color='grey', alpha=0.4)
+
+# for mu in [2.5, 3.5, 4.0]:
+#   X, _ = vdp.dataset(mu, n=n_vdp, b=b_vdp, skip=skip_vdp) # start on limit cycle
+#   X = X[:t]
+#   plt.plot(X[0], X[1], color='green', alpha=0.5, label='Analytic perturbation')
+
+Z0 = obs.extrapolate(nominal.cpu(), X, t)
+axs[0].plot(Z0[0], Z0[1], color='blue')
+axs[0].plot([X[0][0]], [X[1][0]], marker='o', color='blue')
+
+# Sample distribution
+
+axs[1].hist(posterior, bins=int(len(posterior)/4), density=True)
+d = np.linspace(0, 1, 100)
+p = stats.beta.pdf(d, 1.0, beta)
+axs[1].plot(d, p)
+axs[1].set_title(f'Posterior distribution, beta={beta}')
+
+# Closeup
+
+# t = 300
+
+# plt.figure()
+# for Pn in samples:
+# 	Zn = obs.extrapolate(Pn.cpu(), X, t+50)
+# 	plt.plot(Zn[0], Zn[1], color='grey', alpha=0.3)
+
+# plt.xlim(left=X[0][t], right=X[0][0] + 0.05)
+# plt.ylim(bottom=X[1][t], top=X[1][0] + 0.05)
+# plt.title(f'beta={beta}, step={hmc_step}, T={T}, distance={"Frobenius" if baseline else "Kernel"}')
+# Z0 = obs.extrapolate(nominal.cpu(), X, t+100)
+# plt.plot(Z0[0], Z0[1], label='Nominal')
+
+# plt.plot([X[0][0]], [X[1][0]], marker='o', color='blue')
+# plt.legend()
+
+plt.show()
