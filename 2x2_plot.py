21168d51f1d332f43c7c64538e01c3dec7ad24ad
Anand <anand@alphasheets.com>
2020-03-23 02:11:39 -0400

    more systems

--- 2x2_plot.py
+++ 2x2_plot.py
@@ -7,7 +7,7 @@ from sampler.utils import *
 results = hkl.load('saved/2x2.hkl')
 
 n_show = 3
-fig, axs = plt.subplots(3 + n_show, len(results))
+fig, axs = plt.subplots(4 + n_show, len(results))
 
 for i, (name, data) in enumerate(results.items()):
 
@@ -25,15 +25,21 @@ for i, (name, data) in enumerate(results.items()):
 		plot_flow_field(axs[j+1,i], lambda x: S@x, (-4,4), (-4,4))
 
 	# Posterior distribution
-	axs[-2,i].hist(posterior, density=True, bins=max(1, int(len(posterior)/4))) 
+	axs[-3,i].hist(posterior, density=True, bins=max(1, int(len(posterior)/4))) 
 	# axs[-2,i].set_title('Posterior distribution')
 
 	# Trace-determinant plot
-	plot_trace_determinant(axs[-1,i], A, (-4,4), (-4,4))
 	tr = [np.trace(S) for S in samples]
 	det = [np.linalg.det(S) for S in samples]
-	axs[-1,i].scatter(tr, det, color='blue', marker='+', alpha=0.5)
+	axs[-2,i].scatter(tr, det, color='blue', marker='+', alpha=0.5)
+	plot_trace_determinant(axs[-2,i], A, (-4,4), (-4,4))
 	# axs[-1,i].set_title('Trace-determinant plane')
+
+	# Spectral radii
+	radii = [spectral_radius(torch.from_numpy(np.real(diff_to_transferop(s)))).item() for s in samples]
+	radius = spectral_radius(torch.from_numpy(diff_to_transferop(A))).item()
+	axs[-1,i].scatter(radii, np.zeros(len(radii)), marker='+', color='blue')
+	axs[-1,i].scatter([radius], [0], marker='+', color='orange')
 
 fig.suptitle('Perturbations of 2x2 LTI semistable_systems')
 # fig.tight_layout(pad=0.01)
--- systems/lti2x2.py
+++ systems/lti2x2.py
@@ -15,9 +15,11 @@ systems = {
 }
 
 semistable_systems = {
-	'spiral sink': np.array([[-1.,1.],[-1.25, -0.45]]), 
+	'spiral sink1': np.array([[-1.,1.],[-1.25, -0.45]]), 
+	'spiral sink2': np.array([[-1.65,4.],[0.5, -0.4]]), 
 	'nodal sink ': np.array([[-0.05,-3.00],[0.35, -3.45]]), 
-	'center': np.array([[-1.05,-3.60],[1.10, 1.05]]), 
+	'center1': np.array([[-1.05,-3.60],[1.10, 1.05]]), 
+	'center2': np.array([[-0.4,-1.40],[0.50, 0.4]]), 
 }
 
 if __name__ == '__main__': 
