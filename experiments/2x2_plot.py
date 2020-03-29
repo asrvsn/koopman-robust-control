d54682271d4fba857393e4917acc387b14e9aff1
Anand <anand@alphasheets.com>
2020-03-29 17:49:36 -0400

    figures

--- experiments/2x2_plot.py
+++ experiments/2x2_plot.py
@@ -4,60 +4,86 @@ import numpy as np
 
 from sampler.utils import *
 
-method = 'baseline'
-results = hkl.load(f'saved/2x2_{method}.hkl')
+method1, method2 = 'baseline', 'discounted_kernel'
+results1 = hkl.load(f'saved/2x2_{method1}.hkl')
+results2 = hkl.load(f'saved/2x2_{method2}.hkl')
 
-plots = [
-	'spiral_sink', 
-	'spiral_sink2', 
-	'spiral_source', 
-	'nodal_sink ', 
-	'center1', 
-	'center2', 
-	'saddle1', 
-	'saddle2', 
-	'saddle3', 
-	'defective_source',
-	'defective_sink', 
-]
+plots = {
+	# 'spiral_sink', 
+	'spiral_sink2': 'Spiral sink', 
+	'spiral_source': 'Spiral source', 
+	'nodal_sink ': 'Nodal sink', 
+	# 'nodal_source', # too explosive 
+	'center1': 'Center', 
+	'center2': 'Center', 
+	'saddle1': 'Saddle', 
+	# 'saddle2', 
+	# 'saddle3', 
+	# 'defective_source', # distribution not converged
+	# 'defective_sink', 
+}
 
-fig, axs = plt.subplots(4, len(plots))
+fig, axs = plt.subplots(5, len(plots))
 
-for i, name in enumerate(plots):
-
-	data = results[name]
-	A = data['nominal']
-	posterior = data['posterior']
-	samples = data['samples']
+for i, (key, name) in enumerate(plots.items()):
 
 	# Original phase portrait
+	A = results1[key]['nominal']
 	plot_flow_field(axs[0,i], lambda x: A@x, (-4,4), (-4,4))
 	axs[0,i].set_title(name)
-
-	# Perturbed phase portraits
-	# axs[1,i].set_title('Perturbed')
-	# for j, S in enumerate(random.choices(samples, k=n_show)):
-	# 	plot_flow_field(axs[j+1,i], lambda x: S@x, (-4,4), (-4,4))
+	axs[0,i].axis('off')
 
 	# Posterior distribution
+	posterior = results1[key]['posterior']
 	axs[1,i].hist(posterior, density=True, bins=max(1, int(len(posterior)/4))) 
+	axs[1,i].axes.get_yaxis().set_visible(False)
 
 	# Trace-determinant plot
+	samples = results1[key]['samples']
 	tr = [np.trace(S) for S in samples]
 	det = [np.linalg.det(S) for S in samples]
 	axs[2,i].scatter(tr, det, color='blue', marker='+', alpha=0.5)
-	plot_trace_determinant(axs[-2,i], A, (-4,4), (-4,4))
+	plot_trace_determinant(axs[2,i], A, (-4,4), (-4,4))
+	axs[2,i].axes.get_yaxis().set_visible(False)
+	# axs[2,i].axes.get_xaxis().set_visible(False)
 
-	# Spectral radii
-	radii = [spectral_radius(torch.from_numpy(np.real(diff_to_transferop(s)))).item() for s in samples]
-	radius = spectral_radius(torch.from_numpy(diff_to_transferop(A))).item()
-	axs[3,i].scatter(radii, np.zeros(len(radii)), marker='+', color='blue')
-	axs[3,i].scatter([radius], [0], marker='+', color='orange')
+	# # Spectral radii
+	# radii = [spectral_radius(torch.from_numpy(np.real(diff_to_transferop(s)))).item() for s in samples]
+	# radius = spectral_radius(torch.from_numpy(diff_to_transferop(A))).item()
+	# axs[3,i].scatter(radii, np.zeros(len(radii)), marker='+', color='blue')
+	# axs[3,i].scatter([radius], [0], marker='+', color='orange')
+	# axs[3,i].axes.get_yaxis().set_visible(False)
+	# axs[3,i].set_aspect(0.2)
 
-axs[0,0].set_ylabel('phase portrait')
-axs[1,0].set_ylabel('posterior')
-axs[2,0].set_ylabel('trace-determinant')
-axs[3,0].set_ylabel('spectral radii')
+	# Posterior distribution
+	posterior = results2[key]['posterior']
+	axs[3,i].hist(posterior, density=True, bins=max(1, int(len(posterior)/4))) 
+	axs[3,i].axes.get_yaxis().set_visible(False)
+
+	# Trace-determinant plot
+	samples = results2[key]['samples']
+	tr = [np.trace(S) for S in samples]
+	det = [np.linalg.det(S) for S in samples]
+	axs[4,i].scatter(tr, det, color='blue', marker='+', alpha=0.5)
+	plot_trace_determinant(axs[4,i], A, (-4,4), (-4,4))
+	axs[4,i].axes.get_yaxis().set_visible(False)
+	# axs[4,i].axes.get_xaxis().set_visible(False)
+
+	# # Spectral radii
+	# radii = [spectral_radius(torch.from_numpy(np.real(diff_to_transferop(s)))).item() for s in samples]
+	# radius = spectral_radius(torch.from_numpy(diff_to_transferop(A))).item()
+	# axs[6,i].scatter(radii, np.zeros(len(radii)), marker='+', color='blue')
+	# axs[6,i].scatter([radius], [0], marker='+', color='orange')
+	# axs[6,i].axes.get_yaxis().set_visible(False)
+	# axs[6,i].set_aspect(0.2)
+
+# axs[0,0].set_ylabel('phase portrait')
+# axs[1,0].set_ylabel('posterior\n(baseline)')
+# axs[2,0].set_ylabel('trace-determinant\n(baseline)')
+# axs[3,0].set_ylabel('spectral radii\n(baseline)')
+# axs[3,0].set_ylabel('posterior\n(P-F)')
+# axs[4,0].set_ylabel('trace-determinant\n(P-F)')
+# axs[6,0].set_ylabel('spectral radii\n(P-F)')
 
 # fig.suptitle(f'Perturbations of 2x2 LTI semistable_systems ({method})')
 # fig.tight_layout(pad=0.01)
--- experiments/duffing_plot.py
+++ experiments/duffing_plot.py
@@ -15,48 +15,49 @@ results = hkl.load(f'saved/duffing_{method}.hkl')
 nominal = torch.from_numpy(results['nominal']).float()
 posterior = results['posterior']
 samples = [torch.from_numpy(s).float() for s in results['samples']]
-# trajectories = results['trajectories']
+trajectories = results['trajectories']
 
-print('Saving trajectories...')
-n_perturbed = 21
-n_ics = 14
-t = 800
-p, d, k = 5, 2, 15
-obs = PolynomialObservable(p, d, k)
-results['trajectories'] = []
-for P in random.choices(samples, k=n_perturbed):
-	results['trajectories'].append(sample_2d_dynamics(P, obs, t, (-2,2), (-2,2), n_ics, n_ics))
-hkl.dump(results, f'saved/duffing_{method}.hkl')
+# print('Saving trajectories...')
+# n_perturbed = 21
+# n_ics = 14
+# t = 800
+# p, d, k = 5, 2, 15
+# obs = PolynomialObservable(p, d, k)
+# results['trajectories'] = []
+# for P in random.choices(samples, k=n_perturbed):
+# 	results['trajectories'].append(sample_2d_dynamics(P, obs, t, (-2,2), (-2,2), n_ics, n_ics))
+# hkl.dump(results, f'saved/duffing_{method}.hkl')
 
-# def get_color(z):
-# 	if z[0] > -1.5 and z[0] < -0.5 and z[1] > -0.5 and z[1] < 0.5:
-# 		return 'red'
-# 	elif z[0] > 0.5 and z[0] < 1.5 and z[1] > -0.5 and z[1] < 0.5:
-# 		return 'blue'
-# 	else:
-# 		return 'grey'
+def get_color(z):
+	if z[0] > -1.5 and z[0] < -0.2 and z[1] > -0.5 and z[1] < 0.5:
+		return 'red'
+	elif z[0] > 0.2 and z[0] < 1.5 and z[1] > -0.5 and z[1] < 0.5:
+		return 'blue'
+	else:
+		return 'grey'
 
-# y_samples, x_samples = 2, 6
-# fig, axs = plt.subplots(y_samples, x_samples)
-# # fig.suptitle(f'Perturbations of Duffing oscillator ({method})')
-# r, c = 0, 0
-# for perturbed in trajectories:
-# 	ax = axs[r, c]
-# 	xbound, ybound = 2.2, 2.2
-# 	ax.set_xlim(left=-xbound, right=xbound)
-# 	ax.set_ylim(bottom=-ybound, top=ybound)
-# 	ax.axis('off')
-# 	for tr in perturbed:
-# 		ax.plot(tr[0], tr[1], color=get_color(tr[:,-1]))
-# 	c += 1
-# 	if c == x_samples:
-# 		c = 0
-# 		r += 1
-# 		if r == y_samples: 
-# 			break
+y_samples, x_samples = 3, 7
+fig, axs = plt.subplots(y_samples, x_samples)
+# fig.suptitle(f'Perturbations of Duffing oscillator ({method})')
+r, c = 0, 0
+for perturbed in trajectories:
+	ax = axs[r, c]
+	xbound, ybound = 2.2, 2.2
+	ax.set_xlim(left=-xbound, right=xbound)
+	ax.set_ylim(bottom=-ybound, top=ybound)
+	ax.axis('off')
+	for tr in perturbed:
+		ax.plot(tr[0], tr[1], color=get_color(tr[:,-1]))
+	c += 1
+	if c == x_samples:
+		c = 0
+		r += 1
+		if r == y_samples: 
+			break
+plt.tight_layout()
 
-# plt.figure()
-# plt.hist(posterior, density=True, bins=int(len(posterior)/4))
-# plt.title('Posterior distribution')
+plt.figure()
+plt.hist(posterior, density=True, bins=int(len(posterior)/4))
+plt.title('Posterior distribution')
 
-# plt.show()
+plt.show()
