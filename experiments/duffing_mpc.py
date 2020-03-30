faacd887312e379048da1bcf0d1cc4e280e227f1
Anand <anand@alphasheets.com>
2020-03-29 23:21:19 -0400

    odeint solves all

--- experiments/duffing_mpc.py
+++ experiments/duffing_mpc.py
@@ -5,7 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import matplotlib
-matplotlib.use('tkagg')
+from scipy.integrate import odeint
+# matplotlib.use('tkagg')
 
 from sampler.features import *
 from sampler.operators import *
@@ -19,6 +20,11 @@ set_seed(9001)
 p, d, k = 5, 2, 15
 obs = PolynomialObservable(p, d, k)
 
+alpha=-1.0
+beta=1.0
+gamma=0.5
+delta=0.3
+
 def solve_mpc(t0: float, dt: float, x0: torch.Tensor, P: torch.Tensor, B: torch.Tensor, obs: Observable, cost: Callable, h: int, umin=-1., umax=1., eps=1e-5):
 	'''
 	h: horizon
@@ -31,8 +37,9 @@ def solve_mpc(t0: float, dt: float, x0: torch.Tensor, P: torch.Tensor, B: torch.
 	loss, prev_loss = torch.Tensor([float('inf')]), torch.Tensor([0.])
 
 	while torch.abs(loss - prev_loss).item() > eps:
-		x_pred = obs.extrapolate(P, x0.unsqueeze(1), h, B=B, u=u, build_graph=True, unlift_every=False)
+		x_pred = obs.extrapolate(P, x0.unsqueeze(1), h, B=B, u= u, build_graph=True, unlift_every=False)
 		prev_loss = loss
+		# loss = cost(u.clamp(umin, umax), x_pred, window)
 		loss = cost(u, x_pred, window)
 		# print(loss.item())
 		opt.zero_grad()
@@ -46,20 +53,18 @@ def solve_mpc(t0: float, dt: float, x0: torch.Tensor, P: torch.Tensor, B: torch.
 
 def mpc_loop(x0, y0, P, B, obs, cost, h, dt, nmax, tapply=10):
 	history_t = [0]
-	history_x = [[x0, y0]]
 	history_u = [0]
 	t = 0
 	for i in tqdm(range(nmax), desc='MPC'):
 		u_opt = solve_mpc(t, dt, torch.Tensor([x0, y0]), P, B, obs, cost, h)[0]
 		for j in range(tapply):
-			xdot, ydot = duffing.system((x0, y0), None, -1., 1., 0.5, 0.3, lambda _: u_opt[j][0])
+			xdot, ydot = duffing.system((x0, y0), None, alpha, beta, gamma, delta, lambda _: u_opt[j][0])
 			x0 += dt*xdot
 			y0 += dt*ydot
 			t += dt
 			history_t.append(t)
-			history_x.append([x0, y0])
 			history_u.append(u_opt[j][0].item())
-	return np.array(history_t), np.array(history_x).T, np.array(history_u)
+	return np.array(history_t), np.array(history_u)
 
 # Try it
 data = hkl.load('saved/duffing_controlled_nominal.hkl')
@@ -68,15 +73,20 @@ dt = data['dt']
 print('Using dt:', dt)
 
 # xR = lambda t: torch.full(t.shape, -0.5)
-xR = lambda t: torch.cos(t*0.5)
+xR = lambda t: torch.sign(torch.cos(t*0.7))
 cost = lambda u, x, t: ((x[0] - xR(t))**2).sum()
 h = 50
 x0, y0 = 1., 1.
 
-hist_t, hist_x, hist_u = mpc_loop(x0, y0, P, B, obs, cost, h, dt, 100)
+# Re-simulate the system with controls 
+hist_t, hist_u = mpc_loop(x0, y0, P, B, obs, cost, h, dt, 50)
+controller = lambda t: hist_u[int(t/dt)-1]
+hist_x = odeint(duffing.system, (x0, y0), hist_t, args=(alpha, beta, gamma, delta, controller)).T
+
 plt.figure()
-plt.plot(hist_t, hist_x[0], color='blue')
-plt.plot(hist_t, xR(torch.Tensor(hist_t)), color='orange')
+plt.plot(hist_t, hist_x[0], color='blue', label='plant')
+plt.plot(hist_t, xR(torch.Tensor(hist_t)), color='orange', label='reference')
+plt.legend()
 
 plt.figure()
 plt.plot(hist_t, hist_u)
