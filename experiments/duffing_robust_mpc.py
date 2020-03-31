6810a864bd9172a831d2eeba7163b9c6d0e24b0a
Anand <anand@alphasheets.com>
2020-03-30 23:29:59 -0400

    bound uncertainty set

--- experiments/duffing_robust_mpc.py
+++ experiments/duffing_robust_mpc.py
@@ -11,6 +11,7 @@ from scipy.integrate import odeint, ode
 import systems.duffing as duffing
 from sampler.features import *
 from sampler.operators import *
+from sampler.kernel import *
 from experiments.duffing_mpc import mpc_loop
 from experiments.duffing_plot import plot_perturbed, plot_posterior
 
@@ -25,32 +26,51 @@ dt = data['dt']
 print('Using dt:', dt)
 
 data = hkl.load('saved/duffing_uncertainty_set.hkl')
-Ps = [torch.from_numpy(s).float() for s in data['samples']]
 posterior = data['posterior']
-trajectories = data['trajectories']
+# trajectories = data['trajectories']
+samples = [torch.from_numpy(s).float() for s in data['samples']]
 beta = data['beta']
 
-plot_perturbed(trajectories, 2, 6)
-plot_posterior(posterior, beta)
+K = PFKernel(device, P.shape[0], 2, 80)
+dist_func = lambda x, y: K(x, y, normalize=True) 
 
-# # xR = lambda t: torch.full(t.shape, 0.)
-# xR = lambda t: torch.sign(torch.cos(t/4))
-# # xR = lambda t: torch.floor(t/5)/5
-# cost = lambda u, x, t: ((x[0] - xR(t))**2).sum()
-# h = 50
-# x0, y0 = .5, 0.
+# Bound set
+radius = 0.2
+samples = [s for s in samples if dist_func(s, P).item() <= radius]
+posterior = [p for p in posterior if p <= radius]
+
+# # Resample trajectories
+p, d, k = 5, 2, 15
+obs = PolynomialObservable(p, d, k)
+# n_trajectories = 12
+# n_ics = 12
+# t = 800
+# trajectories = [sample_2d_dynamics(s, obs, t, (-2,2), (-2,2), n_ics, n_ics) for s in random.choices(samples, k=n_trajectories)]
+
+# plot_perturbed(trajectories, 2, 6)
+# plot_posterior(posterior, beta)
+
+# Robust MPC
+print('Running MPC...')
+Ps = random.choices(samples, k=20)
+# xR = lambda t: torch.full(t.shape, 0.)
+xR = lambda t: torch.sign(torch.cos(t/4))
+# xR = lambda t: torch.floor(t/5)/5
+cost = lambda u, x, t: ((x[0] - xR(t))**2).sum()
+h = 50
+x0, y0 = .5, 0.
 
 
-# hist_t, hist_u, hist_x = mpc_loop(x0, y0, [P], B, obs, cost, h, dt, 2000)
+hist_t, hist_u, hist_x = mpc_loop(x0, y0, Ps, B, obs, cost, h, dt, 2000)
 
-# results = {
-# 	'dt': dt,
-# 	't': hist_t,
-# 	'u': hist_u,
-# 	'x': hist_x,
-# 	'r': xR(torch.Tensor(hist_t)).numpy(),
-# }
-# print('Saving...')
-# hkl.dump(results, 'saved/duffing_mpc.hkl')
+results = {
+	'dt': dt,
+	't': hist_t,
+	'u': hist_u,
+	'x': hist_x,
+	'r': xR(torch.Tensor(hist_t)).numpy(),
+}
+print('Saving...')
+hkl.dump(results, 'saved/duffing_robust_mpc.hkl')
 
 plt.show()
