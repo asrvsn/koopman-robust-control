f44ce5ec10dd7d0f57ed3d0cfa0f590a10824e9b
Your Name <you@example.com>
2020-04-01 03:14:45 +0000

    vpd

--- experiments/vdp_perturb.py
+++ experiments/vdp_perturb.py
@@ -39,11 +39,11 @@ method = 'baseline'
 # method = 'discounted_kernel'
 
 beta = 5
-step = 1e-6
+step = 5e-5
 leapfrog = 200
 n_samples = 2000
-n_ics = 100
-ic_step = 1e-6
+n_ics = 200
+ic_step = 1e-5
 T = 80
 L = 0.1
 
